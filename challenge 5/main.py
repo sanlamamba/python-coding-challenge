5e8a633bb23a262090ce516bb0b06f9fab4f5955
San Lamamba P <Sanlamamba05@gmail.com>
2022-02-16 16:26:25 +0000

    👌 IMPROVE: Moved main exercice 5 to folder

---
+++ challenge 5/main.py
@@ -0,0 +1,7 @@
+def sWithoutEven(text):
+    return text
+
+if __name__ == "__main__":
+    testCases = ["CODING", "PIZZA", "PYTHON", "A", ""]
+    for i in testCases:
+        print(sWithoutEven(i))
